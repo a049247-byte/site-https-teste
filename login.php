c82cb21f92857ace126727cea7333bd6e1ead837
a049247-byte <a049247@infante.pt>
2026-01-21 12:20:20 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,20 @@
+<?php
+session_start();
+
+/* UTILIZADOR E PASSWORD DA APLICAÇÃO */
+$utilizador_correto = "aluno";
+$password_correta   = "12345";
+
+/* Dados enviados pelo formulário */
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username === $utilizador_correto && $password === $password_correta) {
+    $_SESSION['utilizador'] = $username;
+    header("Location: dashboard.php");
+    exit;
+} else {
+    echo "Login inválido. <a href='index.php'>Tentar novamente</a>";
+}
+?>
 
